26c6d683f28f88a9e04d0a17ac9e9a592b2c40ad
dumygit <dumygit@users.noreply.github.com>
2016-07-09 16:01:27 +0300

    removed unneeded functions

--- src/protobuffer.cpp
+++ src/protobuffer.cpp
@@ -7,126 +7,61 @@
 using namespace std;
 
 /***** Func declarations *****/
-void loadFile(int len, const char* str);
 void ldFile(int len, const char* str);
-void saveToBuffer(int len, const char* str, fstream& fs);
-void testWrite();
 
 /** Global Variables **/
 
 const char* output = "output"; // arbitrary file to (read from)||(save to)
-thread_msg::Thread_Data* tD;
+thread_msg::Thread_Data* tD;   // pointer to msg
 
 /****************************  Functions *****************/
-
-void saveToBuffer(int len, const char* str, fstream& fs)
-{
-	//GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-	cout << "saving to buffer.." << endl;
-	fs << "files saved";
-	tD->set_cluster(str, len);
-}
-
 void ldFile(int len, const char* str)
 {
-	fstream inp(output, ios::in | ios::binary);
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+	fstream inp(output, ios::in | ios::binary); //load file if exists
 	if(!inp.good())
 	{
 		cout << "inp.good #NOT" << endl;
+		inp.open(output, ios::out);   // file does not exist - create file
+		inp.close();
 	}else if (!tD->ParseFromIstream(&inp))
 	{
 		cout << "ParseFromIstream good #NOT" << endl;
 	}else
 	{
 		cout << "all is well" << endl;
-	}
-}
-
-/*
-// loads the predefined arbitrary file "output"
-// if it does not exist, it is created
-// wrapper function for saveToBuffer()
-void loadFile(int len, const char* str)
-{
-	fstream inp(output, ios::in | ios::binary); // loads file
-
-	if (!tD->ParseFromIstream(&inp)){
-		cout << "Failed to write thread_msg" << endl;
-	}
-
-	if(!inp.good())
-	{
-		// file does not exist - create file
-		inp.open(output, ios::out);
-		inp << "files saved";
-		// Debug 
-		//cout << "now? " << inp.good() << endl;
-		//cout << inp << endl;
+		tD->set_cluster(str, len);   // save the new data to a message
+		fstream out(output, ios::out | std::ios::app | ios::binary); // open file to store output
+		if(!out.good())
+		{
+			cout << "out.good #NOT" << endl;
+			// may be superfluous since it should have been created above - added to be safe.
+			// may end up being source of missing input
+			out.open(output, ios::out);   // file does not exist - create file
+			out.close();
+		}
 		
-	}else // file exists - add data at the EoF
-	{
-		cout << inp.good() << endl;
-		//inp.open(output, std::ios::app);
-				
-		inp.close();
-		ofstream out;
-		out.open(output, ios::out | std::ios::app);
-
-		//out << endl;
-//		out << "qfqf";
-		//out << endl;
-		//saveToBuffer(len, str, out);
-
-
-		//cout << inp.good() << endl << inp << endl;
-		
-//	}
-//	saveToBuffer(len, str, inp);
-
-	cout << inp 
-	inp.close();
-
-}
-*/
-
-/*************************** Tests ***********************/
-void testWrite()
-{
-	string str = "Tis a test";
-	//loadFile(str.length(), str.c_str());
-	//ldFile(str.length, str.c_str());
-	cout << "1" << endl;
-	fstream inp(output, ios::in | ios::binary);
-	cout << "2" << endl;
-	cout << inp << endl;
-	if(!inp.good())
-	{
-		cout << "inp.good #NOT" << endl;
-	}
-	//inp.close();
-	inp << "sdfsd" << endl;
-	int temp = tD->cluster.ParseFromIstream(&inp);
-	cout << "3" << endl;
-
-	if (!temp)
-	{
-		cout << "temp #NOT" << endl;
+		if (!tD->SerializeToOstream(&out)) 
+		{
+                	cout << "SerializeToOstream good #NOT" << endl;
+    		}else{
+			cout << "Saved Message to File successfully" << endl;
+		}
 	}
 
-	{
-		cout << "all is well" << endl;
-	}
-	cout << "last" << endl;
+	// Optional:  Delete all global objects allocated by libprotobuf. - from protobuf Documentation
+	google::protobuf::ShutdownProtobufLibrary();
 }
 
-/*************************** Main ************************/
+/*************************** dummy Main for testing only ************************/
 /*
 int main()
 {
 	// test
-	testWrite();
+	const char * str = "Hello Protobuf.";
+	ldFile(strlen(str), str);
 
 	return 0;
 }
-*/
+//*/
