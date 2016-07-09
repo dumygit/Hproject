6dfa4d0096b4014fc700ce9c0f1d479e35c0b504
dumygit <dumygit@users.noreply.github.com>
2016-07-09 16:51:59 +0300

    added debug messages

--- src/protobuffer.cpp
+++ src/protobuffer.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <iostream>  // cout
 #include <fstream>   // file manip
 #include "buf.pb.h"  // protobuf header
@@ -15,39 +14,43 @@ const char* output = "output"; // arbitrary file to (read from)||(save to)
 thread_msg::Thread_Data* tD;   // pointer to msg
 
 /****************************  Functions *****************/
-void ldFile(int len, const char* str)
+void ldFile(size_t len, const char* str)
 {
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-	fstream inp(output, ios::in | ios::binary); //load file if exists
+	cout << "1" << endl;
+	ifstream inp(output, ios::in | ios::binary); //load file if exists
+	cout << "1" << endl;
 	if(!inp.good())
 	{
 		cout << "inp.good #NOT" << endl;
 		inp.open(output, ios::out);   // file does not exist - create file
-		inp.close();
-	}else if (!tD->ParseFromIstream(&inp))
+		cout << inp << endl;
+		//inp.close();
+	}
+	cout << "1" << endl;
+	cout << inp << endl;
+	if (!tD->ParseFromIstream(&inp))
 	{
 		cout << "ParseFromIstream good #NOT" << endl;
-	}else
+	}//else
+
 	{
 		cout << "all is well" << endl;
-		tD->set_cluster(str, len);   // save the new data to a message
-		fstream out(output, ios::out | std::ios::app | ios::binary); // open file to store output
+		cout << "str : " << str << endl;
+		cout << "len : " << len << endl;
+		tD->set_cluster(str);//, len);   // save the new data to a message
+		cout << "aaa" << endl;
+		fstream out(output, ios::out | ios::app | ios::binary); // open file to store output
 		if(!out.good())
 		{
 			cout << "out.good #NOT" << endl;
-			// may be superfluous since it should have been created above - added to be safe.
-			// may end up being source of missing input
-			out.open(output, ios::out);   // file does not exist - create file
-			out.close();
-		}
-		
-		if (!tD->SerializeToOstream(&out)) 
+		}else if (!tD->SerializeToOstream(&out)) 
 		{
                 	cout << "SerializeToOstream good #NOT" << endl;
     		}else{
 			cout << "Saved Message to File successfully" << endl;
 		}
+		out.close();
 	}
 
 	// Optional:  Delete all global objects allocated by libprotobuf. - from protobuf Documentation
